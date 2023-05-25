61cb9d63062a04f123c1a409d2d01353e7944d9e
Kanhaiya <singhkanhaiya91591@gmail.com>
2023-05-25 10:46:41 +0530

    first commit

---
+++ weather-info-website/src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "7713d9056d87a5755498ece69a3d64f0",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
